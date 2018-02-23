1f7f163f6a7c28774bbb769173adf9f590cfdd60
Luke Deighton <LukeDeighton1@gmail.com>
2018-02-23 22:11:49 +0000

    Fix client side reading of blockchain

--- client/src/main/scala/com/github/lukedeighton/highcoin/ReadWriters.scala
+++ client/src/main/scala/com/github/lukedeighton/highcoin/ReadWriters.scala
@@ -8,11 +8,20 @@ import upickle.{Js, default}
 
 object ReadWriters {
 
-  implicit def date: RW[Date] = RW(date => Js.Str(date.toString), {
+  implicit def dateRW: RW[Date] = RW(date => Js.Str(date.toString), {
     case jsDate => new Date(jsDate.str.toString)
   })
 
-  implicit def transactionOutputRW: RW[Transaction.Output] = default.macroRW
+  implicit def transactionOutputRW: RW[Transaction.Output] =
+    RW(default.macroW[Transaction.Output].write, transactionOutputRead)
+
+  def transactionOutputRead: PartialFunction[Js.Value, Transaction.Output] = { // doesn't like BigDecimal
+    case outputObj: Js.Obj =>
+      val output = outputObj.value.toMap
+      val value = BigDecimal(output("value").num.toString)
+      val address = output("address").str.toString
+      Transaction.Output(value, address)
+  }
 
   implicit def transactionOutputRefRW: RW[Transaction.OutputRef] = default.macroRW
 
@@ -20,7 +29,18 @@ object ReadWriters {
 
   implicit def transactionRW: RW[Transaction] = default.macroRW
 
-  implicit def blockRW: RW[Block] = default.macroRW
+  implicit def blockRW: RW[Block] = RW(default.macroW[Block].write, blockRead)
+
+  def blockRead: PartialFunction[Js.Value, Block] = { //All because I couldn't work out Options
+    case blockObj: Js.Obj =>
+      val block = blockObj.value.toMap
+      val height = block("height").num.asInstanceOf[Int]
+      val nonce = block("nonce").num.asInstanceOf[Int]
+      val transactions = block("transactions").arr.map(transactionRW.read)
+      val previousHash = block.get("previousHash").map(_.str.toString)
+      val timestamp = dateRW.read(block("timestamp"))
+      Block(height, nonce, transactions, previousHash, timestamp)
+  }
 
   implicit def blockchainRW: RW[Blockchain] = default.macroRW
 
